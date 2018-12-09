1ff7c52319339d292b91213e59fa85b1f2dccc2b
Mindaugas <mliktas@gmail.com>
2018-12-09 23:30:29 +0200

    Creating a Service (last tasks)

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -7,9 +7,9 @@ export class ProductService {
 
   private _albumUrl = '../assets/album.json';
 
-  constructor(private _hhtp: Http) { }
+  constructor(private _http: Http) { }
 
   getAlbum(id: number){
-    return this._hhtp.get(this._albumUrl).map((response) => response.json());
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
 }
